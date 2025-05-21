f1c03b3be2e8e12ef646ca5d4b338bebdc19fb44
fazli2008 <aushafrauffazli@gmail.com>
2025-05-21 13:56:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 fazliweh ganteng")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("5ca4c8d77eef78ebb59b918b3fe8a8c9_2.jpeg", width=200)
